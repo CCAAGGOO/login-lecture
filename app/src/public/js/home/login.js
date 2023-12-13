e488a3af44ae9135154122776883075291bd8a82
CCAAGGOO <rmfls427@gmail.com>
2023-12-13 18:08:25 +0900

    fetch

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -4,12 +4,18 @@ const $id = document.querySelector("#id"),
       $pw = document.querySelector("#pw"),
       $loginBtn = document.querySelector("button");
 
-  $loginBtn.addEventListener('click', login);
+$loginBtn.addEventListener('click', login);
 
-  function login () {
-    const req = {
-      id: $id.value,
-      pw: $pw.value,
-    };
-    console.log(req);
+function login () {
+  const req = {
+    id: $id.value,
+    pw: $pw.value,
   };
+  fetch("/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(req),
+  });
+};
