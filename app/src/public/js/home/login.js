ea16fd2d532dbf93d722d8b6b3cb7dbdcb4aec43
CCAAGGOO <rmfls427@gmail.com>
2023-12-13 18:25:36 +0900

    로그인

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -17,5 +17,5 @@ function login () {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(req),
-  });
+  }).then((res) => res.json()).then(console.log);
 };
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -9,9 +9,27 @@ const output = {
   },
 };
 
+const users = {
+  id: ["abab", "김호석"],
+  pw: ["1234", "1234"],
+};
+
 const process = {
   login: (req, res) => {
-    console.log(req.body);
+    const id = req.body.id;
+    const pw = req.body.pw;
+    if (users.id.includes(id)) {
+      const idx = users.id.indexOf(id);
+      if (users.pw[idx] === pw) {
+        return res.json({
+          success: true,
+        });
+      }
+    }
+    return res.json({
+      success: false,
+      msg: "로그인에 실패하셨습니다.",
+    });
   },
 };
 
